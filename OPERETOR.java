05d062be3b45e63c69d8754805d032e99a6e1cb5
Bilal-Khanzar <bilalkhanzer985@gmail.com>
2024-11-09 09:35:27 -0800

    Add files via upload
    unite 2 Java if else classes

---
+++ OPERETOR.java
@@ -0,0 +1,247 @@
+package operetorler;
+
+import java.nio.channels.AsynchronousSocketChannel;
+import java.util.Scanner;
+
+public class OPERETOR {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * if else if ve else  operetor ile baslariz 
+		 * switch case or break if e benzer bir sistem burada donuyor 
+		 * for dongulu ve while dongusu ile 
+		 * 
+		 */
+		
+		 Scanner scanner = new Scanner(System.in);
+		 
+		 /*System.out.println("enter first num1 ");
+			int num1 = scanner.nextInt();
+			
+			 System.out.println("enter secound  num2 ");
+			int num2 = scanner.nextInt();
+			int num3 =( num1 + num2)/2 ;
+			if (num3>85) {
+				System.out.println("bu kisi sinifin birincisidir "+num3);
+				
+			}else if (num3> 50) {
+				System.out.println("%50 puan almis yani sinifin ikincisi " +num3);
+				
+			}
+			else {
+				System.out.println("daha dusuk not aldin ");
+			}*/
+		
+//		 ==============================================================================================================================================
+//                                                 PROJE BANKA HESAP PARA YYATIRMA CEKME 
+		
+		 
+		 /* int para = 40000;
+		         String fake_name = "ahmet".toUpperCase();
+		         int password = 501349777;
+
+		         System.out.println("Please enter your name, sir:");
+		         String name = scanner.nextLine().toUpperCase();
+
+		         System.out.println("Please enter your password, sir:");
+		         int password1 = scanner.nextInt();
+
+		         if (fake_name.equals(name)) { // String karşılaştırmasını equals ile yapıyoruz
+		             if (password == password1) {
+		                 System.out.println("HOS GELDINIZ, SAYIN MUSTERI");
+		                 System.out.println("1- Para cekme; 2- Para yatirma; 3- Cikis");
+
+		                 int islem = scanner.nextInt();
+		                 if (islem == 1) {
+		                     if (para == 0) {
+		                         System.out.println("Bakiyeniz olmadigi icin para cekmeniz mumkun degildir. Bakiye: " + para);
+		                     } else {
+		                         System.out.println("Cekmek istediginiz para miktarini giriniz:");
+		                         int cek = scanner.nextInt();
+
+		                         para -= cek;
+
+		                         System.out.println("Para cekme isleminiz basarili olmustur. Kalan bakiye: " + para);
+		                     }
+		                 } else if (islem == 2) {
+		                     System.out.println("Yatirmak istediginiz para miktarini giriniz:");
+		                     int ekle = scanner.nextInt();
+
+		                     para += ekle;
+
+		                     System.out.println("Bankadaki yeni para miktari: " + para);
+		                 } else {
+		                     System.out.println("Isleminiz bitmistir, kartiniz iade edildi.");
+		                 }
+		             } else {
+		                 System.out.println("Sifre yanlis. Lutfen tekrar deneyiniz.");
+		             }
+		         } else {
+		             System.out.println("Isim yanlis. Lutfen tekrar deneyiniz.");
+		         }
+
+		         scanner.close(); // Scanner kaynak sızıntısını önlemek için kapatılmalı   
+		         */
+		  
+
+		 
+		 
+		 
+		 /*
+		 String day = "thursday";
+		 switch (day) {
+		 
+		 case "sunday":
+			 System.out.println("pazar gunudur ");
+			 break;
+			 
+		 case "monday":
+			 System.out.println("pazartesi  gunudur ");
+			 break;
+			 
+			 
+		 case "tuesday":
+			 System.out.println("sali gunudur ");
+			 break;
+			 
+			 
+			 
+		 case "wednesday":
+			 System.out.println("carsamba gunudur ");
+			 break;
+			 
+			 
+			 
+		 case "thursday":
+			 System.out.println("persembe gunudur ");
+			 break;
+			 
+			 
+			 
+		 case "friday ":
+			 System.out.println("cuma gunudur ");
+			 break;
+			 
+			 
+			 
+		 case "saturday":
+			 System.out.println("cumartesi gunudur ");
+			 break;
+			    
+		 
+		 
+		 
+		 
+		 
+		 
+		 }
+		*/
+		 
+		 
+		 
+		 
+		 
+		 
+//		 ================================================ FORDONGUSU===============================================================
+		 
+//		 int a = 20;
+//		 int toplam = 0;
+//		 for(int i =0 ; i<=20; i++ ) {
+//			 toplam +=i;
+//			
+//			
+//		 } 
+//System.out.println("toplam deger : " +toplam);
+		 
+		 // agic kodu 
+		 
+		/* for(int i =0; i<6 ; i++) {
+			  for(int j=0 ; j<=i ; j++) {
+				  System.out.print("* " );
+			  }
+			 System.out.println();
+			
+		 }*/
+		 
+//		 for(int i =0; i <25; i++) {
+//			 System.out.println(i);
+//		 }
+
+		 
+	
+		
+
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+
+		}
+
+	}
+
+
+
