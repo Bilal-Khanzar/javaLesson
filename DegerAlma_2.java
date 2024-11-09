8340569a3976acf55f97b819623f29af2d55e411
Bilal-Khanzar <bilalkhanzer985@gmail.com>
2024-11-09 09:28:34 -0800

    Add files via upload
    Unit1 input output

---
+++ DegerAlma_2.java
@@ -0,0 +1,137 @@
+
+package degerAlma_2;
+
+import java.util.Scanner; // when you want to get input, first use this import line
+
+public class DegerAlma_2 {
+
+    public static void main(String[] args) {
+        // Create a Scanner object
+        Scanner scanner = new Scanner(System.in);
+
+      /*  // Prompt the user for input
+        System.out.print("Enter your age: ");
+
+        // Read the input
+        int age = scanner.nextInt();
+
+        // Print the input
+        System.out.println("Your age is: " + age);
+
+       */
+        
+        
+        
+        /*
+        System.out.println("Hello Sr can you spell your beautiful name ?");
+        
+        String name = scanner.next();
+        System.out.println(" Sr can you give me your surname ?");
+       
+        String surname = scanner.next();
+        
+        System.out.println(" thanks sir , sir last question how old is you ?");
+        int age = scanner.nextInt();
+        
+        System.out.println(" thanks for gived to me your detail now i repeat your name is  " + name +"your surname is "+ surname + "your age is  "+ age);
+        
+        */
+        
+        /*
+        // i take int  numbers then i changed to str
+        System.out.println("enter a and b numer ");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        
+       String c = String.valueOf(a) ;
+       String d = String.valueOf(a) ;
+       String e = c + d;
+       
+       System.out.println(" our number changed to string  "+ e);
+        
+        */
+        
+        
+        //  NOT  bir int degerden aldiktan sonra bir alt satirda write canner.line maathod after that you can use str input 
+        
+        
+        
+        System.out.println("bir int deger gir ");
+        int z = scanner.nextInt();
+        double b = Math.sqrt(z) * 2;
+        System.out.println(b);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // Close the scanner
+        scanner.close();
+    }
+}
