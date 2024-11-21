9216855b2a3680531d521ffdee829e119db6ae00
Bilal-Khanzar <bilalkhanzer985@gmail.com>
2024-11-21 04:27:05 -0800

    Add files via upload

---
+++ Cok_boyutlu_Array.java
@@ -0,0 +1,134 @@
+package Cok_boyutlu;
+
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Cok_boyutlu_Array {
+
+	public static void main(String[] args) {
+		// cok boyutlu Array 
+		
+		// bunlar satir ve  sutunden olusuyor  bu bir onceki dersi kalan kisimlarioda tekrar etmemiz gerekir mesela dictionry vs 
+		
+		  
+		
+		/*
+		 * int[][] TwoBoyutlu = new int[2][2] [] satir [] sutun ; 
+		int[][] threeBoyutlu = new int[3][3];
+		int[][] forBoyutlu = new int[4][4];
+		int[][] besBoyutlu = new int[5][5];
+		*/
+		
+		/*
+		int[][] ikiboyutlu =new int[] []{
+			{12,23,2},
+			{11,24,36},
+			{15,84,46},
+			{23,64,5}
+			
+		};
+		for(int i = 0 ; i< ikiboyutlu.length ; i++) {
+			for(int j=0 ; j<ikiboyutlu[0].length ; j++) {
+				System.out.print(ikiboyutlu[i] [j] + " ");
+			}
+			System.out.println();
+		}
+		*/
+		Scanner scanner = new Scanner(System.in);
+//		System.out.print("a icin satir degeri giriniz ");
+//		int a = scanner.nextInt();
+//		System.out.print("b icin sutun degeri giriniz ");
+//		int b = scanner.nextInt();
+//		System.out.println("");
+	
+		/*int ikiBoyutlu[][] = new int[4][4];
+		System.out.println("iki boyutlu dizi giriniz ");
+	for(int i= 0 ; i<ikiBoyutlu.length ; i++) {
+		for(int j=0 ; j<ikiBoyutlu[0].length; j++) {
+			
+			System.out.print("matrix[" + i + "][" + j + "] = ");
+            ikiBoyutlu[i][j] = scanner.nextInt();
+			
+		}
+
+	}
+	System.out.println("girilen metrix dizi bu sekilde ");
+	for(int i = 0; i<ikiBoyutlu.length; i++) {
+		for (int j = 0 ; j<ikiBoyutlu.length ;j++) {
+			
+			 System.out.print(ikiBoyutlu[i][j] + " ");
+		}
+		System.out.println();
+	}
+	*/
+		
+		
+		/* dizi icin ornek olsun 
+		 * 
+		 * for(int k=0;k<7; k++) {
+		 *
+			for(int l =0; l<7; l++) {
+				System.out.println(k + "" +l + "");
+			}
+		}*/ 
+		
+		
+		int [][] 
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
