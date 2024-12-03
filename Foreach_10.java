2d292f67844c960ba7f63c2d567f06f63b1ab550
Bilal-Khanzar <bilalkhanzer985@gmail.com>
2024-12-03 01:52:19 -0800

    Add files via upload
    foreach_method

---
+++ Foreach_10.java
@@ -0,0 +1,99 @@
+package foreach_10;
+
+
+import java.util.Collections;
+import java.util.Random;
+import java.util.Arrays;
+
+public class Foreach_10 {
+
+	public static void main(String[] args) {
+		// foreach döngüsü, yalnızca Java'nın Iterable arayüzünü uygulayan sınıflar için kullanılabilir.
+		//Örneğin, ArrayList, HashSet, LinkedList, TreeSet gibi yapılar bu arayüzü uygular.
+	
+		// fora benzer tum islemleri iki satir icinde yapar 
+		
+		
+//	int[] a = {1,3,4,5,6,4,3,23,2,4,345};
+//	for (int i : a) {
+//		System.out.println(i);
+//	}
+//	
+		
+		// isim siralama iicin kullanilir 
+	/*
+	 * String[] bS = {"ahmet","naween ","akbar aga","davut","hamza"};  
+	 *
+	System.out.println(bS[0]);
+	for (String i : bS) {
+		   Arrays.sort(bS);
+		System.out.println(i);
+	}*/
+		// bir icin icinde rastgegele sayilar atariz 0 ile 18 arasnda 6 diziden olusacak 
+	/*	int Dizi[] = new  int[6];
+		
+		for(int i= 0 ; i< Dizi.length;i++) {
+			Random random = new Random();
+			Integer sec = random.nextInt(18);
+			Dizi[i] = sec;
+			
+			}
+		for (int j : Dizi) {
+			System.out.println("result  " + j);
+		}
+	
+	
+	*/
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
---
+++ methods_and_fanction_12.java
@@ -0,0 +1,90 @@
+package foreach_10;
+
+import java.security.PublicKey;
+
+public class methods_and_fanction_12 {
+
+	public static void main(String[] args) {
+		//methods and fanction 
+		
+
+		
+		/*
+		
+		public class MyClass {
+
+		    // 1. Metot burada tanımlanabilir
+		    public static int toplama(int a, int b) {
+		        return a + b;
+		    }
+
+		    // 2. Ana metot
+		    public static void main(String[] args) {
+		        System.out.println("Sonuç: " + toplama(3, 7));
+		    }
+
+		    // 3. Başka bir metot
+		    public static void selamVer() {
+		        System.out.println("Merhaba!");
+		    }
+		}*/ // fonksiyon tanima sekli 
+		
+		/*
+		 System.out.println("my first function ");
+		 toplama(4, 10);
+		 int d = toplama(5, 10);
+		 System.out.println(" our function result is  " + d);
+
+		
+		
+	
+		
+		
+		
+	}
+	public static int toplama(int a, int b) {
+		int c ;
+		c = a + b;
+		
+        return c;
+    }
+	}*/
+		/*
+		int a = 5;
+		int fak = 1;
+		for(int i= 1; i<=a; i++) {
+			fak *= i;
+			
+		}
+		System.out.println(fak);
+		*/
+		
+		// parametre gondermeden ciktisi cikartmasi 
+		
+	     System.out.println(topla());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	public static String topla() {
+		int a = 23;
+		int b = 43;
+		int c = a + b;
+		return( "our result is " + c);
+	}
+	}
