478a60805e0e0561cd5950cd738a20a5d93ca70a
Bilal-Khanzar <bilalkhanzer985@gmail.com>
2024-12-03 01:53:06 -0800

    Add files via upload
    finally

---
+++ main.java
@@ -0,0 +1,134 @@
+package finallay;
+
+class Arac {
+    public void calistir() {
+        System.out.println("Araç çalışıyor.");
+    }
+}
+
+class Araba extends Arac {
+    @Override
+    public void calistir() {
+        System.out.println("Araba çalışıyor.");
+    }
+}
+
+public class main {
+    public static void main(String[] args) {
+//        Arac arac = new Arac();
+//        Arac araba = new Araba();
+
+//        arac.calistir(); // Araç çalışıyor.
+//        araba.calistir(); // Araba çalışıyor.
+//        
+        
+    	
+    	
+    	
+    	
+    	
+    	/*
+    	 * 
+    	 * class UstSinif {
+    public final void sabitMetot() {
+        System.out.println("Bu metot değiştirilemez.");
+    }
+}
+
+class AltSinif extends UstSinif {
+    // public void sabitMetot() { // Hata: final metot override edilemez
+    //     System.out.println("Değiştirmeye çalışıyoruz.");
+    // }
+}
+
+    	 */
+        
+        
+        
+        /*
+         * 
+         * 
+         * public class ParametreOrnek {
+    public void mesajYaz(final String mesaj) {
+        // mesaj = "Yeni Mesaj"; // Hata: final parametre değiştirilemez
+        System.out.println(mesaj);
+    }
+
+    public static void main(String[] args) {
+        ParametreOrnek ornek = new ParametreOrnek();
+        ornek.mesajYaz("Merhaba, dünya!");
+    }
+}
+
+         */
+    	
+    	/*
+    	 * 
+    	 * 
+    	 * Static ne dir 
+    	 * 
+    	 * sayisal metod ici nkullanilir return olan 
+    	 * toplam degerleri toplamak icin kullanilir mesela kac kisi girdi kac kisi cikti orada 
+    	 * 
+    	 * sinif icinde deger nesnelereen bagimsiz sinif uyelerini tanimlamak icin kullanilir 
+    	 * 
+    	 */
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+}
+
+// finally @overrifde kullanilamaz 
+// static te kullanilamaz 
+// finally sabit bir degiskendir degismeyen
+
